5f40ce9d2a5e9650f18dfc4f2a35ca83d0e8b076
Andrea Guerrieri <aguerrieri@eusoft.net>
2024-10-15 23:08:04 +0200

    fix andrid release build (don't know how) hti view mesh builder fix compute index box collider normals fix pymeshcollider dount3d progress logger source

--- android-build.cmd
+++
@@ -1,4 +0,0 @@
-
-SET NDK_PROJECT_PATH=%CD%
-
-call C:\Android\ndk\26.1.10909125\build\ndk-build
--- jni/Android.mk
+++ jni/Android.mk
@@ -6,12 +6,13 @@ LOCAL_MODULE := etcpack
 
 LOCAL_CFLAGS += -Wno-implicit-function-declaration
 LOCAL_CPPFLAGS += -D__ARM_NEON
-
+LOCAL_LDFLAGS += $(ANDROID_LD_FLAGS)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../zlib
 
 LOCAL_SRC_FILES	:= 	$(wildcard $(LOCAL_PATH)/../../zlib/*.c) \
 					$(wildcard $(LOCAL_PATH)/../*.cpp) \
+			        $(wildcard $(LOCAL_PATH)/../*.c) \
 					$(wildcard $(LOCAL_PATH)/../libpng/*.c) \
 					$(wildcard $(LOCAL_PATH)/../getopt/*.c) \
 					$(wildcard $(LOCAL_PATH)/../lz4/*.c) 
