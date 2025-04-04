95008297f3e00d2856dde2427811c68582faefad
Andrea Guerrieri <aguerrieri@eusoft.net>
2025-04-04 21:10:28 +0200

    native libs ugrade / sync

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,12 +8,14 @@ LOCAL_CFLAGS += -Wno-implicit-function-declaration
 LOCAL_CPPFLAGS += -D__ARM_NEON
 LOCAL_LDFLAGS += $(ANDROID_LD_FLAGS)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../zlib
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../zlib \
+				    $(LOCAL_PATH)/../libpng
 
 LOCAL_SRC_FILES	:= 	$(wildcard $(LOCAL_PATH)/../../zlib/*.c) \
 					$(wildcard $(LOCAL_PATH)/../*.cpp) \
 			        $(wildcard $(LOCAL_PATH)/../*.c) \
 					$(wildcard $(LOCAL_PATH)/../libpng/*.c) \
+					$(wildcard $(LOCAL_PATH)/../libpng/arm/*.c) \
 					$(wildcard $(LOCAL_PATH)/../getopt/*.c) \
 					$(wildcard $(LOCAL_PATH)/../lz4/*.c) 
 					
